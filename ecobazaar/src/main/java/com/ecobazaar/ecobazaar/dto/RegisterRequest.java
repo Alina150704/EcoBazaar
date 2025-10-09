43f418449f3d91aab04dab90c2ece3de9f02cba3
alina <alina@Alina>
2025-10-09 15:56:08 +0530

    Initial commit

---
+++ ecobazaar/src/main/java/com/ecobazaar/ecobazaar/dto/RegisterRequest.java
@@ -0,0 +1,33 @@
+package com.ecobazaar.ecobazaar.dto;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+public class RegisterRequest {
+	@NotBlank(message=" name is required")
+	private String name;
+	@NotBlank(message="email is required")
+	@Email(message="enter a valid email")
+	private String email;
+	@NotBlank(message="password is required")
+	@Size(min=4,message="must be  atleast 4 characters")
+	private String password;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
